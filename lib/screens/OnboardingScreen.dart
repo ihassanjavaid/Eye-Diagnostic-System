bb5fd4b60e2d701136f4d76c35bc2c1e8c80ccbc
ihassanjavaid <great.maliks@gmail.com>
2020-08-25 12:07:47 +0500

    onboardingscreen_init

---
+++ lib/screens/OnboardingScreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  @override
+  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.1, 0.4, 0.7, 0.9],
+            colors: [
+              Color(0xFF3594DD),
+              Color(0xFF4563DB),
+              Color(0xFF5036D5),
+              Color(0xFF5b16D0)
+            ],
+          ),
+        ),
+      ),
+    )
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:eye_diagnostic_system/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(EyeSee());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
