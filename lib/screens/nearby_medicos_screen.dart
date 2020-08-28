6c6b727258fb560a97c02afa10ce9c82a3e9c684
ihassanjavaid <great.maliks@gmail.com>
2020-08-28 14:28:34 +0500

    update

---
+++ lib/screens/nearby_medicos_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NearbyMedicos extends StatefulWidget {
+  @override
+  _NearbyMedicosState createState() => _NearbyMedicosState();
+}
+
+class _NearbyMedicosState extends State<NearbyMedicos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
