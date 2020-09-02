6e59a544a948e5c389e2f4a72a3d91342b418354
ihassanjavaid <great.maliks@gmail.com>
2020-09-02 22:02:13 +0500

    Update menu_screen.dart

--- lib/screens/eye_sight_screens/menu_screen.dart
+++ lib/screens/eye_sight_screens/menu_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'duo_chrome_screen.dart';
 
 class Menu extends StatelessWidget {
-  static const String id = 'Menu';
+  static const String id = 'vision_testing_menu_screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
