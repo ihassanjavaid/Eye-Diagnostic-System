1b7313894bd341e3e7b071ab2ab9a485e080d213
ihassanjavaid <great.maliks@gmail.com>
2020-09-09 02:06:40 +0500

    Update provider_data.dart

--- lib/models/provider_data.dart
+++ lib/models/provider_data.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/cupertino.dart';
 
 class ProviderData extends ChangeNotifier{
+  //=== for forums ===
   String _tagData ='';
   String _questionData = '';
   String _questionID = '';
+
+  String get tagData => _tagData;
+  String get questionData => _questionData;
+  String get questionID => _questionID;
 
   void updateTagData(String tag){
     this._tagData = tag;
     notifyListeners();
   }
 
-  String get tagData => _tagData;
-
   void updateQuestionData(String question){
     this._questionData = question;
     notifyListeners();
   }
 
-  String get questionData => _questionData;
-
   void updateQuestionID(String id){
     this._questionID = id;
     notifyListeners();
   }
-  String get questionID => _questionID;
+
+  //=== for assistant ===
 }
