5e4fae66ec505d131e85eae391cc656c2b6b915b
Mahnoor Fatima Saad <mfc2496@gmail.com>
2020-09-09 14:54:48 +0500

    Update forum_screen.dart

--- lib/screens/community_screens/forum_screen.dart
+++ lib/screens/community_screens/forum_screen.dart
@@ -39,6 +39,9 @@ class _ForumState extends State<Forum> {
   List<Question> _questions = [];
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
   FirestoreUserService _userService = FirestoreUserService();
+  bool _userPressed = false;
+  bool _tagPressed = false;
+
   //String selectedTagItem = '';
 
 
@@ -57,12 +60,25 @@ class _ForumState extends State<Forum> {
     return qn.docs;
   }
 
+  Future getUserPosts() async{
+    FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    SharedPreferences _pref = await SharedPreferences.getInstance();
+    QuerySnapshot qn = await _firestore.collection('questions').where('email', isEqualTo: _pref.getString('email')).get();
+    return qn.docs;
+  }
+
   Expanded choosePosts() {
-    if (Provider.of<ProviderData>(context).tagData == '') {
-      return buildExpandedQuestionSection(context);
-    } else {
+    if(_userPressed){
+      setState(() {
+        _tagPressed = false;
+      });
+      return buildExpandedUserPostsSection(context);
+    }
+    else if(_tagPressed){
       return buildExpandedTagPostsSection(context);
     }
+    else
+      return buildExpandedQuestionSection(context);
   }
 
 
@@ -216,6 +232,8 @@ class _ForumState extends State<Forum> {
                 itemBuilder: (_, index) {
                   return GestureDetector(
                     onTap: () {
+                      Provider.of<ProviderData>(context, listen: false).updateQuestionData(snapshot.data[index].data()['question']);
+                      Provider.of<ProviderData>(context, listen: false).updateQuestionID(snapshot.data[index].id);
                       Navigator.pushNamed(context, ForumDetails.id);
                     },
                     child: Container(
@@ -304,6 +322,113 @@ class _ForumState extends State<Forum> {
     );
   }
 
+  Expanded buildExpandedUserPostsSection(BuildContext context) {
+    return Expanded(
+      child: FutureBuilder(
+          future: getUserPosts(),
+          builder: (_, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: kTealColor,
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (_, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Provider.of<ProviderData>(context, listen: false).updateQuestionData(snapshot.data[index].data()['question']);
+                      Provider.of<ProviderData>(context, listen: false).updateQuestionID(snapshot.data[index].id);
+                      Navigator.pushNamed(context, ForumDetails.id);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                color: kAmberColor,
+                                width: 2,
+                              ))),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 25.0,
+                          backgroundColor: kTealColor.withOpacity(0.8),
+                          child: FutureBuilder(
+                              future: _userService.getUserInitial(
+                                  email: snapshot.data[index].data()['email']),
+                              builder: (_, snapshot2) {
+                                if (snapshot2.hasData) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(top: 4.0),
+                                    child: Text(
+                                      snapshot2.data,
+                                      style: kAvatarTextStyle,
+                                    ),
+                                  );
+                                } else {
+                                  return Center(
+                                      child: CircularProgressIndicator(
+                                        backgroundColor: kTealColor,
+                                      ));
+                                }
+                              }),
+                          foregroundColor: kTealColor,
+                        ),
+                        title: Padding(
+                          padding:
+                          const EdgeInsets.only(left: 2.0, bottom: 5.0),
+                          child: Text(
+                            snapshot.data[index].data()['question'],
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: kTealColor),
+                          ),
+                        ),
+                        subtitle: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            height: 20.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(20.0),
+                                    bottomRight: Radius.circular(20.0)),
+                                color: kTealColor),
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 8.0, right: 8.0, top: 1.5),
+                              child: Text(
+                                snapshot.data[index].data()['tag'],
+                                style: TextStyle(
+                                  //fontWeight: FontWeight.bold,
+                                    color: kLightTealColor),
+                              ),
+                            ),
+                          ),
+                        ),
+                        trailing: Chip(
+                          backgroundColor: kLightAmberColor,
+                          shape: BeveledRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          label: Text(
+                            snapshot.data[index].data()['views'].toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: kLightTealColor),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          }),
+    );
+  }
+
   Widget _buildTopPanel() {
     return ClipPath(
       clipper: HeaderCustomClipper(),
@@ -350,7 +475,9 @@ class _ForumState extends State<Forum> {
                       children: [
                         GestureDetector(
                           onTap: () async {
-                            _questions = await _getQuestions();
+                            setState(() {
+                              _userPressed = true;
+                            });
                             // Navigator.pushNamed(context, ForumUserQuestions.id);
                           },
                           child: Icon(
@@ -388,6 +515,10 @@ class _ForumState extends State<Forum> {
                       children: [
                         GestureDetector(
                           onTap: () async {
+                            setState(() {
+                              _tagPressed = true;
+                              _userPressed = false;
+                            });
                             await tgsdlg.showCard(context);
                           },
                           child: Icon(
@@ -400,6 +531,28 @@ class _ForumState extends State<Forum> {
                           height: 5.0,
                         ),
                         Text('Tags', style: kforumHeaderButtonLabelStyle),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () async {
+                            setState(() {
+                              _userPressed = false;
+                              _tagPressed = false;
+                              Provider.of<ProviderData>(context, listen: false).updateTagData('');
+                            });
+                          },
+                          child: Icon(
+                            Icons.refresh_rounded,
+                            color: kAmberColor,
+                            size: 34.0,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text('Refresh', style: kforumHeaderButtonLabelStyle),
                       ],
                     ),
                   ],
