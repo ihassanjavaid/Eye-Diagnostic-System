6d2f1ce5a320301c6221bb75b5f1e5f959e81594
MUHAMMAD OSAMA SALEEM <33492161+IAMOSAMA111@users.noreply.github.com>
2020-09-23 23:02:23 +0500

    contrast sensitivity screen 1 added

--- lib/screens/eye_sight_screens/contrast_sensitivity/contrast_screen1.dart
+++ lib/screens/eye_sight_screens/contrast_sensitivity/contrast_screen1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:eye_diagnostic_system/components/header_clipper_component.dart';
 
 class ContrastScreen1 extends StatelessWidget {
   static const String id = 'ContrastScreen1';
@@ -9,103 +10,106 @@ class ContrastScreen1 extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text("Contrast Sesitivity"),
-        centerTitle: true,
-
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Card(
-                child: Image(
-                  image: AssetImage('assets/images/contrast/night.gif'),
-                ),
-              ),
+      body: Column(
+        children: [
+          Container(
+            child: ClipPath(
+              clipper: HeaderCustomClipper(),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Container(
-                child: Column(
-                  children: [
-                    Text("Problems while driving in the rain or at night",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Card(
+                    child: Image(
+                      image: AssetImage('assets/images/contrast/night.gif'),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RaisedButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            onPressed: (){},
-                            color: Colors.redAccent,
-                            child: Text("Often",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold
-                              ),
-                            ),
-
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          RaisedButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            onPressed: (){},
-                            color: Colors.blue,
-                            child: Text("Sometime",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          RaisedButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            onPressed: (){},
-                            color: Colors.green,
-                            child: Text("Never",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-
-                          ),
-
-                        ],
-                      ),
-                    )
-
-                  ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Text("Problems while driving in the rain or at night",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                onPressed: (){},
+                                color: Colors.redAccent,
+                                child: Text("Often",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                                  ),
+                                ),
+
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                onPressed: (){},
+                                color: Colors.blue,
+                                child: Text("Sometime",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                onPressed: (){},
+                                color: Colors.green,
+                                child: Text("Never",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+
+                              ),
+
+                            ],
+                          ),
+                        )
+
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
