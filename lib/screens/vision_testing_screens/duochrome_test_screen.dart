5d5258c6cb47dee8f23ecc4ae4ba44015a9df2d6
Mahnoor Fatima Saad <mfc2496@gmail.com>
2021-03-18 14:25:42 +0500

    _duochrome_test_UI

--- lib/screens/vision_testing_screens/duochrome_test_screen.dart
+++ lib/screens/vision_testing_screens/duochrome_test_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DuochromeTestScreen extends StatefulWidget {
   static const String id = 'duochrome_test_screen';
@@ -7,8 +9,142 @@ class DuochromeTestScreen extends StatefulWidget {
 }
 
 class _DuochromeTestScreen extends State<DuochromeTestScreen> {
+  String hyperopiaGreen01 = 'assets/images/vision_testing/duo-chrome tests/green01.png';
+  String hyperopiaGreen02 = 'assets/images/vision_testing/duo-chrome tests/green02.png';
+  String hyperopiaYellow01 = 'assets/images/vision_testing/duo-chrome tests/yellow01.png';
+  String hyperopiaYellow02 = 'assets/images/vision_testing/duo-chrome tests/yellow02.png';
+
+  String myopiaRed01 = 'assets/images/vision_testing/duo-chrome tests/red01.png';
+  String myopiaRed02 = 'assets/images/vision_testing/duo-chrome tests/red02.png';
+  String myopiaOrange01 = 'assets/images/vision_testing/duo-chrome tests/orange01.png';
+  String myopiaOrange02 = 'assets/images/vision_testing/duo-chrome tests/orange02.png';
+
+  String myopiaImage = '';
+  String hyperopiaImage = '';
+
+  int hyperopiaPressed = 0;
+  int myopiaPressed = 0;
+
+  String imageProgressionHyper(int pressed){
+    if (pressed == 0){
+      hyperopiaImage = hyperopiaGreen01;
+    }
+    else if(pressed == 1){
+      hyperopiaImage = hyperopiaGreen02;
+    }
+    else if(pressed == 2){
+      hyperopiaImage = hyperopiaYellow01;
+    }
+    else{
+      hyperopiaImage = hyperopiaYellow02;
+    }
+
+    return hyperopiaImage;
+  }
+  String imageProgressionMyopia(int pressed){
+    if (pressed == 0){
+      myopiaImage = myopiaRed01;
+    }
+    else if(pressed == 1){
+      myopiaImage = myopiaRed02;
+    }
+    else if(pressed == 2){
+      myopiaImage = myopiaOrange01;
+    }
+    else{
+      myopiaImage = myopiaOrange02;
+    }
+
+    return myopiaImage;
+  }
+
+  Widget _buildTopBox(image) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2.15,
+      width: double.infinity,
+      child: FittedBox(
+        child: Image(
+          image: AssetImage(image),
+        ),
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  Widget _buildMiddlePanel() {
+    return Container(
+      color: Colors.black87,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.arrow_forward,
+            size: 50,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomBox(image) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2.15,
+      width: double.infinity,
+      child: FittedBox(
+        child: Image(
+          image: AssetImage(image),
+        ),
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    myopiaImage = myopiaRed01;
+   // hyperopiaImage = hyperopiaGreen01;
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Column(
+          children: [
+            GestureDetector(
+              child: _buildTopBox(imageProgressionHyper(hyperopiaPressed)),
+              onTap: () {
+                hyperopiaPressed = hyperopiaPressed + 1;
+                setState(() {
+                  print(hyperopiaImage);
+                  hyperopiaImage = imageProgressionHyper(hyperopiaPressed);
+                  print(hyperopiaImage);
+                });
+              },
+            ),
+            GestureDetector(
+              child: _buildMiddlePanel(),
+              onTap: (){
+                hyperopiaPressed += 1;
+                myopiaPressed += 1;
+                setState(() {
+                  myopiaImage = imageProgressionMyopia(myopiaPressed);
+                  hyperopiaImage = imageProgressionHyper(hyperopiaPressed);
+                });
+              },
+            ),
+            Expanded(
+              child: GestureDetector(
+                child: _buildBottomBox(imageProgressionMyopia(myopiaPressed)),
+                onTap: () {
+                  myopiaPressed = myopiaPressed + 1;
+                  setState(() {
+                    myopiaImage = imageProgressionMyopia(myopiaPressed);
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
