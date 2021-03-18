30b9053c5ae0901ca16458d2c1bc576607b211c6
Mahnoor Fatima Saad <mfc2496@gmail.com>
2021-03-18 17:06:25 +0500

    _duochrome UI

--- lib/screens/vision_testing_screens/duochrome_test_screen.dart
+++ lib/screens/vision_testing_screens/duochrome_test_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:eye_diagnostic_system/components/header_clipper_component.dart';
+import 'package:eye_diagnostic_system/screens/vision_testing_screens/vision_testing_main.dart';
 import 'package:eye_diagnostic_system/utilities/constants.dart';
+import 'package:eye_diagnostic_system/widgets/alert_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -10,15 +15,23 @@ class DuochromeTestScreen extends StatefulWidget {
 }
 
 class _DuochromeTestScreen extends State<DuochromeTestScreen> {
-  String hyperopiaGreen01 = 'assets/images/vision_testing/duo-chrome tests/green01.png';
-  String hyperopiaGreen02 = 'assets/images/vision_testing/duo-chrome tests/green02.png';
-  String hyperopiaYellow01 = 'assets/images/vision_testing/duo-chrome tests/yellow01.png';
-  String hyperopiaYellow02 = 'assets/images/vision_testing/duo-chrome tests/yellow02.png';
+  String hyperopiaGreen01 =
+      'assets/images/vision_testing/duo-chrome tests/green01.png';
+  String hyperopiaGreen02 =
+      'assets/images/vision_testing/duo-chrome tests/green02.png';
+  String hyperopiaYellow01 =
+      'assets/images/vision_testing/duo-chrome tests/yellow01.png';
+  String hyperopiaYellow02 =
+      'assets/images/vision_testing/duo-chrome tests/yellow02.png';
 
-  String myopiaRed01 = 'assets/images/vision_testing/duo-chrome tests/red01.png';
-  String myopiaRed02 = 'assets/images/vision_testing/duo-chrome tests/red02.png';
-  String myopiaOrange01 = 'assets/images/vision_testing/duo-chrome tests/orange01.png';
-  String myopiaOrange02 = 'assets/images/vision_testing/duo-chrome tests/orange02.png';
+  String myopiaRed01 =
+      'assets/images/vision_testing/duo-chrome tests/red01.png';
+  String myopiaRed02 =
+      'assets/images/vision_testing/duo-chrome tests/red02.png';
+  String myopiaOrange01 =
+      'assets/images/vision_testing/duo-chrome tests/orange01.png';
+  String myopiaOrange02 =
+      'assets/images/vision_testing/duo-chrome tests/orange02.png';
 
   String myopiaImage = '';
   String hyperopiaImage = '';
@@ -26,33 +39,28 @@ class _DuochromeTestScreen extends State<DuochromeTestScreen> {
   int hyperopiaPressed = 0;
   int myopiaPressed = 0;
 
-  String imageProgressionHyper(int pressed){
-    if (pressed == 0){
+  String imageProgressionHyper(int pressed) {
+    if (pressed == 0) {
       hyperopiaImage = hyperopiaGreen01;
-    }
-    else if(pressed == 1){
+    } else if (pressed == 1) {
       hyperopiaImage = hyperopiaGreen02;
-    }
-    else if(pressed == 2){
+    } else if (pressed == 2) {
       hyperopiaImage = hyperopiaYellow01;
-    }
-    else{
+    } else {
       hyperopiaImage = hyperopiaYellow02;
     }
 
     return hyperopiaImage;
   }
-  String imageProgressionMyopia(int pressed){
-    if (pressed == 0){
+
+  String imageProgressionMyopia(int pressed) {
+    if (pressed == 0) {
       myopiaImage = myopiaRed01;
-    }
-    else if(pressed == 1){
+    } else if (pressed == 1) {
       myopiaImage = myopiaRed02;
-    }
-    else if(pressed == 2){
+    } else if (pressed == 2) {
       myopiaImage = myopiaOrange01;
-    }
-    else{
+    } else {
       myopiaImage = myopiaOrange02;
     }
 
@@ -60,69 +68,177 @@ class _DuochromeTestScreen extends State<DuochromeTestScreen> {
   }
 
   Widget _buildTopBox(image) {
-    return Container(
-      height: MediaQuery.of(context).size.height / 2.15,
-      width: double.infinity,
-      child: FittedBox(
-        child: Image(
-          image: AssetImage(image),
+    return Padding(
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 2.75,
+        width: MediaQuery.of(context).size.width / 1.15,
+        child: FittedBox(
+          child: Image(
+            image: AssetImage(image),
+          ),
+          fit: BoxFit.fill,
         ),
-        fit: BoxFit.fill,
       ),
     );
   }
 
   Widget _buildMiddlePanel() {
-    return Container(
-      color: kScaffoldBackgroundColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Both Appear the Same',
-            style: kOnBoardingSubtitleStyle. copyWith(color: Colors.black87, fontSize: 30),
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(left:30.0, right: 20.0, bottom: 10),
+      child: Container(
+        color: kScaffoldBackgroundColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: (){
+                Navigator.popAndPushNamed(context, VisionTestingMain.id);
+              },
+              child: Icon(
+                Icons.home,
+                size: 30,
+                color: kDarkTealColor,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                AlertWidget()
+                    .generateInstructions(context: context,
+                    title: 'Instructions',
+                    description: 'Tap on the colour which appears clearer to you and on the forward arrow if both are equally clear.')
+                    .show();
+              },
+              child: Icon(
+                Icons.help_outline,
+                size: 30,
+                color: kDarkTealColor,
+              ),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              size: 30,
+              color: kDarkTealColor,
+            )
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildBottomBox(image) {
-    return Container(
-      height: MediaQuery.of(context).size.height / 2.15,
-      width: double.infinity,
-      child: FittedBox(
-        child: Image(
-          image: AssetImage(image),
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 2.75,
+        width: MediaQuery.of(context).size.width / 1.15,
+        child: FittedBox(
+          child: Image(
+            image: AssetImage(image),
+          ),
+          fit: BoxFit.fill,
         ),
-        fit: BoxFit.fill,
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    myopiaImage = myopiaRed01;
-   // hyperopiaImage = hyperopiaGreen01;
     return Scaffold(
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: Column(
           children: [
-            GestureDetector(
-              child: _buildTopBox(imageProgressionHyper(hyperopiaPressed)),
-              onTap: () {
-                hyperopiaPressed = hyperopiaPressed + 1;
-                setState(() {
-                  print(hyperopiaImage);
-                  hyperopiaImage = imageProgressionHyper(hyperopiaPressed);
-                  print(hyperopiaImage);
-                });
-              },
+            // Container(
+            //   color: kTealColor,
+            //   height: 50,
+            // ),
+            ClipPath(
+              clipper: HeaderCustomClipper(),
+              child: Column(children: [
+                Container(
+                  color: kTealColor,
+                  padding: const EdgeInsets.only(
+                      left: 15, right: 15, top: 50.0, bottom: 40.0),
+                  alignment: Alignment.center,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(children: [
+                            TextSpan(
+                              text: 'Duo-Chrome\t',
+                              style: kDashboardTitleTextStyle.copyWith(
+                                  color: kAmberColor),
+                            ),
+                            TextSpan(
+                              text: 'Test',
+                              style: kDashboardTitleTextStyle.copyWith(
+                                  color: kAmberColor),
+                            ),
+                          ]),
+                        ),
+                      ]),
+                ),
+              ]),
+            ),
+            // ClipRRect(
+            //   borderRadius: BorderRadius.only(
+            //       bottomLeft: Radius.circular(50),
+            //       bottomRight: Radius.circular(50)),
+            //   child: Container(
+            //     color: kTealColor,
+            //     height: 140,
+            //     child: Row(
+            //       mainAxisAlignment: MainAxisAlignment.center,
+            //       children: [
+            //         Text(
+            //           'Duo-Chrome Test',
+            //           style: kOnBoardingTitleStyle.copyWith(fontSize: 30),
+            //         )
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(60),
+              child: Container(
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      child:
+                          _buildTopBox(imageProgressionHyper(hyperopiaPressed)),
+                      onTap: () {
+                        hyperopiaPressed = hyperopiaPressed + 1;
+                        setState(() {
+                          print(hyperopiaImage);
+                          hyperopiaImage =
+                              imageProgressionHyper(hyperopiaPressed);
+                          print(hyperopiaImage);
+                        });
+                      },
+                    ),
+                    GestureDetector(
+                      child: _buildBottomBox(
+                          imageProgressionMyopia(myopiaPressed)),
+                      onTap: () {
+                        myopiaPressed = myopiaPressed + 1;
+                        setState(() {
+                          myopiaImage = imageProgressionMyopia(myopiaPressed);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(),
             ),
             GestureDetector(
               child: _buildMiddlePanel(),
-              onTap: (){
+              onTap: () {
                 hyperopiaPressed += 1;
                 myopiaPressed += 1;
                 setState(() {
@@ -130,17 +246,6 @@ class _DuochromeTestScreen extends State<DuochromeTestScreen> {
                   hyperopiaImage = imageProgressionHyper(hyperopiaPressed);
                 });
               },
-            ),
-            Expanded(
-              child: GestureDetector(
-                child: _buildBottomBox(imageProgressionMyopia(myopiaPressed)),
-                onTap: () {
-                  myopiaPressed = myopiaPressed + 1;
-                  setState(() {
-                    myopiaImage = imageProgressionMyopia(myopiaPressed);
-                  });
-                },
-              ),
             ),
           ],
         ),
--- lib/widgets/alert_widget.dart
+++ lib/widgets/alert_widget.dart
@@ -211,5 +211,32 @@ class AlertWidget{
       ],
     );
   }
+  generateInstructions({ @required context, @required title, @required description}){
+    return Alert(
+      context: context,
+      type: AlertType.info,
+      title: title,
+      desc: description,
+      buttons: [
+        DialogButton(
+          color: kTealColor,
+          child: Text(
+            "Try Again",
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          onPressed: () {
+            /*
+            * popping the context 2 times
+            * first pops the dialog
+            * second pops the loading screen and returns back to the sign-in/reg screen
+            */
+            Navigator.pop(context);
+            Navigator.pop(context);
+          },
+          width: 130,
+        )
+      ],
+    );
+  }
 
 }
